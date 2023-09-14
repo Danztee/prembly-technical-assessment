b56bafd42dcf28366a0c70a10d5e5075cae209cc
Olowoniyi Daniel <olowoniyidaniel@gmail.com>
2023-09-14 11:39:17 +0100

    fixed slider bug

--- exercise-2-image-slider/src/components/ImageSlider.jsx
+++ exercise-2-image-slider/src/components/ImageSlider.jsx
@@ -108,7 +108,7 @@ const ImageSlider = () => {
       </section>
 
       <div className="controls">
-        <div onClick={prevSlide}>
+        <button onClick={prevSlide} className="prev">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
@@ -122,9 +122,9 @@ const ImageSlider = () => {
               d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
             />
           </svg>
-        </div>
+        </button>
 
-        <div onClick={nextSlide}>
+        <button onClick={nextSlide} className="next">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
@@ -138,19 +138,10 @@ const ImageSlider = () => {
               d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
             />
           </svg>
-        </div>
+        </button>
       </div>
     </>
   );
 };
 
 export default ImageSlider;
-
-{
-  /* <span
-                  className="visibility-icon"
-                  onClick={() => disableVisibility(slide.id)}
-                >
-                  👁️
-                </span> */
-}
